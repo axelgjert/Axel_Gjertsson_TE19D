2a12fa67075872a90f0f15faec9b4bee05a7ccea
axelgjert <axel.gjertsson@elev.ga.ntig.se>
2020-09-02 12:12:22 +0200

    commit

---
+++ Variabler/variabler.py
@@ -0,0 +1,11 @@
+name = "Axel" # har skapat variablen name och tilldelat det värdet "Axel"
+age = 16 #skapa variablen age och tilldelat det värdet 16
+print(f"Hej {name} du är {age} år gammal.")
+
+sida = float(input("Ange kvadratens sida: "))
+
+area = sida**2
+
+omkrets = 4*sida
+
+print(f"Kvadratens area är {area} a.e. och kvadratens omkrets är {omkrets} l.e. ")
---
+++ Variabler/Är talet större.py
@@ -0,0 +1,9 @@
+tal = int(input("Skriv ett tal:"))
+if tal < 40:
+    print("Talet är mindre än 40.")
+else:
+    print("Talet är större än 40")
+if tal < 100:
+    print("Talet är däremot mindre än 100")
+
+    
